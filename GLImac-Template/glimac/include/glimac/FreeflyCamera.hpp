4907aeab2b60eb4c001c641cfd5bc4066c93363a
hydai <huiying.dai@edu.esiee.fr>
2023-01-23 17:44:13 +0100

    project base

---
+++ GLImac-Template/glimac/include/glimac/FreeflyCamera.hpp
@@ -0,0 +1,60 @@
+#pragma once
+
+#include <glm/glm.hpp>
+#include <vector>
+#include <string>
+
+
+namespace glimac {
+
+class FreeflyCamera{
+private : 
+	glm::vec3 m_Position; // Position de la caméra
+	float m_fPhi; // Coordonnées sphériques
+	float m_fTheta; // Coordonnées sphériques
+	
+	glm::vec3 m_FrontVector; // Vecteur F
+	glm::vec3 m_LeftVector; // Vecteur L
+	glm::vec3 m_UpVector; // Vecteur U
+	
+	void computeDirectionVectors(){
+		m_FrontVector =glm::vec3(glm::cos(m_fTheta)*glm::sin(m_fPhi),glm::sin(m_fTheta),glm::cos(m_fTheta)*glm::cos(m_fPhi)); 
+		m_LeftVector =glm::vec3(glm::sin(m_fPhi+(glm::pi<float>()/2)),0,glm::cos(m_fPhi+(glm::pi<float>()/2))); 
+		m_UpVector = glm::cross(m_FrontVector, m_LeftVector);
+	}
+
+public : 
+	FreeflyCamera(){
+		m_Position = glm::vec3(0,0,0);
+    	m_fPhi=glm::pi<float>();
+    	m_fTheta=0;
+    	computeDirectionVectors();
+    }
+    
+    void moveLeft(float t){
+    	m_Position += t*m_LeftVector;
+    	computeDirectionVectors();
+    }
+    
+    void moveFront(float t){
+    	m_Position += t*m_FrontVector;
+    	computeDirectionVectors();
+    }
+    
+    void rotateLeft(float degrees){
+    	m_fTheta = m_fTheta + glm::radians(degrees);
+    	computeDirectionVectors();
+    }
+    void rotateUp(float degrees){
+    	m_fPhi = m_fPhi + glm::radians(degrees);
+    	computeDirectionVectors();
+    }
+    
+    glm::mat4 getViewMatrix() const{
+    	glm::vec3 point = m_Position+m_FrontVector;
+    	return glm::lookAt(m_Position, point, m_UpVector);
+    }
+
+};
+
+}
---
+++ GLImac-Template/glimac/include/glimac/TrackballCamera.hpp
@@ -0,0 +1,44 @@
+#pragma once
+
+#include <glm/glm.hpp>
+#include <vector>
+#include <string>
+
+
+namespace glimac {
+
+class TrackballCamera {
+private:
+	float m_fDistance;
+	float m_fAngleX;
+	float m_fAngleY;
+	
+public:
+    TrackballCamera(){
+    	m_fDistance=5;
+    	m_fAngleX=0;
+    	m_fAngleY=0;
+    }
+    
+    void moveFront(float delta){
+    
+    	m_fDistance=m_fDistance+delta;
+    } //permettant d'avancer / reculer la caméra. Lorsque delta est positif la caméra doit avancer, sinon elle doit reculer.
+	void rotateLeft(float degrees){
+		m_fAngleX=m_fAngleX+degrees;
+	} //permettant de tourner latéralement autour du centre de vision.
+	void rotateUp(float degrees){
+		m_fAngleY=m_fAngleY+degrees;
+	}//permettant de tourner verticalement autour du centre de vision.
+	
+	glm::mat4 getViewMatrix() const{		
+		glm::mat4 MatriceFinal = glm::translate(glm::mat4(1), glm::vec3(0, 0, -m_fDistance)); // Translation
+		MatriceFinal = glm::rotate(MatriceFinal, glm::radians(m_fAngleX), glm::vec3(0, 1, 0)); // Translation * Rotation
+		MatriceFinal = glm::rotate(MatriceFinal, glm::radians(m_fAngleY), glm::vec3(1, 0, 0)); // Translation * Rotation * Rotation
+		return MatriceFinal;
+	}
+	
+
+};
+
+}
